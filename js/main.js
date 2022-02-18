c49a14d339a3bd789ef901878434d5136eb65b8a
martineho <martineolsenn@hotmail.com>
2022-02-18 19:15:23 +0530

    fixed render products

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 import { getProducts } from "./ui/getProducts.js";
 import { renderProducts } from "./ui/renderProducts.js";
-import { filterProducts } from "./ui/filterProducts.js";
+// import { filterProducts } from "./ui/filterProducts.js";
 
-const search = document.querySelector(".search__form__input");
+// const search = document.querySelector(".search__form__input");
+const productContainer = document.querySelector(".product-container");
 
 async function main() {
     const products = await getProducts();
+    productContainer.innerHTML = "";  
     renderProducts(products);
-    search.addEventListener('onchange', function(event) {
-      const filteredProducts = filterProducts(products);
-      renderProducts(filteredProducts);
-    })
+    //search.addEventListener('change', function(event) {
+     // const filteredProducts = filterProducts(products);
+     // renderProducts(filteredProducts);
+   // })
   }
   
   main();
--- js/ui/renderProducts.js
+++ js/ui/renderProducts.js
@@ -1,23 +1,18 @@
 
-
 export function renderProduct(product) {
 
     const productContainer = document.querySelector(".product-container");
-
-    productContainer.innerHTML = "";
-    
-    for(let i = 0; i < product.length; i++ ){
-    
+   
     const productName = product.title;
     const productPrice = product.price;
 
     productContainer.innerHTML += `<div class="product">
                                         <h4>${productName}</h4>
                                         <div class="price">${productPrice}</div>
-                                        </div>`;
-    }
+                                        </div>`;                                
 }
 
 export function renderProducts(listOfProducts) {
     listOfProducts.forEach(renderProduct);
+    productContainer.innerHTML = "";
   }
